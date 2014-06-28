20e1576bbae78bac1e3fd25a37b76cd3e072cb32
runt9 <runt9ab@gmail.com>
2014-06-28 03:36:33 -0500

    [#74081066] Frontend Players Prototype - Wire up table to be sortable in an Angular way

--- static/javascript/controllers.js
+++ static/javascript/controllers.js
@@ -18,6 +18,16 @@ angular.module('PlayersApp.controllers', []).controller('playersController', fun
            games_played: "16",
            espn_ranking: "2",
            last_season_points: "500"
+       },
+       {
+           name: "LeSean McCoy",
+           position: "RB",
+           nfl_team: "PHI",
+           games_played: "16",
+           espn_ranking: "3",
+           last_season_points: "499"
        }
    ];
+   $scope.predicate = "name";
+   $scope.reverse = true;
 });
