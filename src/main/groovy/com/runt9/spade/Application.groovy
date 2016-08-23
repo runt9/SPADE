664fb94dd826a6bbcb882a83419d2149f05be424
Alex Vester <avester@getvalify.com>
2016-08-23 13:33:38 -0500

    Keep it simple for assets, just get it working

--- src/main/groovy/com/runt9/spade/Application.groovy
+++ src/main/groovy/com/runt9/spade/Application.groovy
@@ -3,11 +3,13 @@ package com.runt9.spade
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan(['com.runt9.spade', 'asset.pipeline.springboot'])
 class Application {
-    static main(args) {
-        SpringApplication.run(Application.class, args)
+    static void main(String[] args) {
+        SpringApplication.run(Application, args)
     }
 }
